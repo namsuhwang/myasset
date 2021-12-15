c89292e4649142a878fea54e5c220449043f257e
Hwang Namsu <namsuhwang@naver.com>
2021-12-15 12:57:48 +0900

    2021-12-15 #1

---
+++ src/main/java/com/idlelife/myasset/MyassetApplication.java
@@ -0,0 +1,13 @@
+package com.idlelife.myasset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyassetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyassetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/idlelife/myasset/MyassetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idlelife.myasset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyassetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
